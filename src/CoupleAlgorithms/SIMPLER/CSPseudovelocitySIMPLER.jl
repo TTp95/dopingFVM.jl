5b192a15da1f8bc874f3925f6c5d57ddd14e3da1
Felipe A. Díaz <studio.pipe.95@gmail.com>
2021-11-03 20:25:15 -0300

    SIMPLER & SIMPLERnP

--- src/CoupleAlgorithms/SIMPLER/CSPseudovelocitySIMPLER.jl
+++ src/CoupleAlgorithms/SIMPLER/CSPseudovelocitySIMPLER.jl
@@ -4,66 +4,64 @@
 function SIMPLER_pseudovelocity end
 
 function SIMPLER_pseudovelocity(
-    x::Array{<:AbstractFloat,1},
     AU::Union{
     SparseVector{<:AbstractFloat,<:Signed},
     SparseMatrixCSC{<:AbstractFloat,<:Signed},
     Array{<:AbstractFloat,2},
     },
+    bU::Array{<:AbstractFloat,1},
     velocity::CSVelocity1D,
     mesh::UnionCSMesh1D;
     relaxP = 1.0,
     T::Type{<:AbstractFloat} = Float64,
+    full::Bool = true,
 )
-    array_pc = zeros(T, mesh.l1)
-    array_DU = zeros(T, mesh.l1)
-    array_gradpc = zeros(T, mesh.l1)
+    pseudo_u = zeros(mesh.l1)
 
-    # vector to array
-    vector_to_phi!(x, velocity.p, mesh; phisolution = array_pc)
+    u = phi_to_vector(velocity.u, mesh)
 
-    # pressure correction gradient (result = vector)
-    vector_pc = pressure_phi_gradient(velocity.p, mesh; phisolution = array_pc, T = T)
-
-    # vector to array (easy manipulation for next step)
-    vector_to_phi!(vector_pc, velocity.p, mesh; phisolution = array_gradpc)
+    asum_u = 0.0
 
     for i in 1:mesh.l1
-        if velocity.u.onoff[i]
+        if (velocity.u.onoff[i] == 1.0)
+
             id = velocity.u.gIndex[i]
-            array_DU[i] = 1.0/AU[id,id]
-        else
-            array_DU[i] = 0.0
+
+            asum_u = 0.0
+
+            if i != 1
+                if (velocity.u.onoff[i-1] == 1.0)
+                    asum_u += AU[id, velocity.u.gIndex[i-1]] * u[velocity.u.gIndex[i-1]]
+                end
+            end
+
+            if i != mesh.l1
+                if (velocity.u.onoff[i+1] == 1.0)
+                    asum_u += AU[id, velocity.u.gIndex[i+1]] * u[velocity.u.gIndex[i+1]]
+                end
+            end
+
+            if full
+                if i > 2
+                    if (velocity.u.onoff[i-2] == 1.0)
+                        asum_u += AU[id, velocity.u.gIndex[i-2]] * u[velocity.u.gIndex[i-2]]
+                    end
+                end
+
+                if i < mesh.l1-1
+                    if (velocity.u.onoff[i+2] == 1.0)
+                        asum_u += AU[id, velocity.u.gIndex[i+2]] * u[velocity.u.gIndex[i+2]]
+                    end
+                end
+
+            end
+
+            pseudo_u[i] = (bU[id] - asum_u)/(AU[id,id])
+
         end
     end
 
-    # Pressure
-    velocity.p.eval = velocity.p.eval + (relaxP * array_pc)
-
-    # Velocity
-    velocity.u.eval = velocity.u.eval - (array_DU .* array_gradpc)
-
-    # Face Velocity
-    for i in 2:mesh.l1
-        if velocity.u.onoff[i] && velocity.u.onoff[i-1]
-            D1 = mesh.vol[i]/AU[velocity.u.gIndex[i],velocity.u.gIndex[i]]
-            D2 = mesh.vol[i-1]/AU[velocity.u.gIndex[i-1],velocity.u.gIndex[i-1]]
-
-            num = D1 * (mesh.dx[i-1] * 0.5) + D2 * (mesh.dx[i] * 0.5)
-            den = 0.5 * (mesh.dx[i-1] + mesh.dx[i])
-            Df = num / den
-
-            num = array_pc[i] - array_pc[i-1]
-            den = mesh.x[i] - mesh.x[i-1]
-            grad_press_face = num / den
-
-            press = Df * grad_press_face
-
-            velocity.fValues.uFace[i] = velocity.fValues.uFace[i] - press
-        end
-    end
-
-    return nothing
+    return pseudo_u
 end
 
 function SIMPLER_pseudovelocity(
@@ -83,43 +81,96 @@ function SIMPLER_pseudovelocity(
     mesh::UnionCSMesh2D;
     relaxP = 1.0,
     T::Type{<:AbstractFloat} = Float64,
+    full::Bool = true,
 )
     pseudo_u = zeros(mesh.l1, mesh.m1)
     pseudo_v = zeros(mesh.l1, mesh.m1)
-    ac_AU = 0.0
-    ac_AV = 0.0
 
     u = phi_to_vector(velocity.u, mesh)
     v = phi_to_vector(velocity.v, mesh)
 
+    asum_u = 0.0
+    asum_v = 0.0
 
     for i in 1:mesh.l1
         for j in 1:mesh.m1
             if (velocity.u.onoff[i,j] == 1.0)
+
                 id = velocity.u.gIndex[i,j]
-                if (AU[id,id] != 0.0) && (AV[id,id] != 0.0)
-                    ac_AU = AU[id,id]
-                    ac_AV = AV[id,id]
 
-                    AU[id,id] = 0.0
-                    AV[id,id] = 0.0
+                asum_u = 0.0
+                asum_v = 0.0
 
-                    pseudo_u[i,j] = (bU[id] - sum(@views (AU[id,:]) .* u))/(ac_AU)
-                    pseudo_v[i,j] = (bV[id] - sum(@views (AV[id,:]) .* v))/(ac_AV)
-
-                    AU[id,id] = ac_AU
-                    AV[id,id] = ac_AV
+                if i != 1
+                    if (velocity.u.onoff[i-1,j] == 1.0)
+                        asum_u += AU[id, velocity.u.gIndex[i-1,j]] * u[velocity.u.gIndex[i-1,j]]
+                        asum_v += AV[id, velocity.v.gIndex[i-1,j]] * v[velocity.v.gIndex[i-1,j]]
+                    end
                 end
+
+                if i != mesh.l1
+                    if (velocity.u.onoff[i+1,j] == 1.0)
+                        asum_u += AU[id, velocity.u.gIndex[i+1,j]] * u[velocity.u.gIndex[i+1,j]]
+                        asum_v += AV[id, velocity.v.gIndex[i+1,j]] * v[velocity.v.gIndex[i+1,j]]
+                    end
+                end
+
+                if j != 1
+                    if (velocity.u.onoff[i,j-1] == 1.0)
+                        asum_u += AU[id, velocity.u.gIndex[i,j-1]] * u[velocity.u.gIndex[i,j-1]]
+                        asum_v += AV[id, velocity.v.gIndex[i,j-1]] * v[velocity.v.gIndex[i,j-1]]
+                    end
+                end
+
+                if j != mesh.m1
+                    if (velocity.u.onoff[i,j+1] == 1.0)
+                        asum_u += AU[id, velocity.u.gIndex[i,j+1]] * u[velocity.u.gIndex[i,j+1]]
+                        asum_v += AV[id, velocity.v.gIndex[i,j+1]] * v[velocity.v.gIndex[i,j+1]]
+                    end
+                end
+
+                if full
+                    if i > 2
+                        if (velocity.u.onoff[i-2,j] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i-2,j]] * u[velocity.u.gIndex[i-2,j]]
+                            asum_v += AV[id, velocity.v.gIndex[i-2,j]] * v[velocity.v.gIndex[i-2,j]]
+                        end
+                    end
+
+                    if i < mesh.l1-1
+                        if (velocity.u.onoff[i+2,j] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i+2,j]] * u[velocity.u.gIndex[i+2,j]]
+                            asum_v += AV[id, velocity.v.gIndex[i+2,j]] * v[velocity.v.gIndex[i+2,j]]
+                        end
+                    end
+
+                    if j > 2
+                        if (velocity.u.onoff[i,j-2] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i,j-2]] * u[velocity.u.gIndex[i,j-2]]
+                            asum_v += AV[id, velocity.v.gIndex[i,j-2]] * v[velocity.v.gIndex[i,j-2]]
+                        end
+                    end
+
+                    if j < mesh.m1-1
+                        if (velocity.u.onoff[i,j+2] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i,j+2]] * u[velocity.u.gIndex[i,j+2]]
+                            asum_v += AV[id, velocity.v.gIndex[i,j+2]] * v[velocity.v.gIndex[i,j+2]]
+                        end
+                    end
+
+                end
+
+                pseudo_u[i,j] = (bU[id] - asum_u)/(AU[id,id])
+                pseudo_v[i,j] = (bV[id] - asum_v)/(AV[id,id])
+
             end
         end
     end
-
 
     return pseudo_u, pseudo_v
 end
 
 function SIMPLER_pseudovelocity(
-    x::Array{<:AbstractFloat,1},
     AU::Union{
     SparseVector{<:AbstractFloat,<:Signed},
     SparseMatrixCSC{<:AbstractFloat,<:Signed},
@@ -135,137 +186,145 @@ function SIMPLER_pseudovelocity(
     SparseMatrixCSC{<:AbstractFloat,<:Signed},
     Array{<:AbstractFloat,2},
     },
+    bU::Array{<:AbstractFloat,1},
+    bV::Array{<:AbstractFloat,1},
+    bW::Array{<:AbstractFloat,1},
     velocity::CSVelocity3D,
     mesh::UnionCSMesh3D;
     relaxP = 1.0,
     T::Type{<:AbstractFloat} = Float64,
+    full::Bool = true,
 )
-    array_pc = zeros(T, mesh.l1, mesh.m1, mesh.n1)
-    array_DUx = zeros(T, mesh.l1, mesh.m1, mesh.n1)
-    array_DUy = zeros(T, mesh.l1, mesh.m1, mesh.n1)
-    array_DUz = zeros(T, mesh.l1, mesh.m1, mesh.n1)
-    array_gradpcx = zeros(T, mesh.l1, mesh.m1, mesh.n1)
-    array_gradpcy = zeros(T, mesh.l1, mesh.m1, mesh.n1)
-    array_gradpcz = zeros(T, mesh.l1, mesh.m1, mesh.n1)
+    pseudo_u = zeros(mesh.l1, mesh.m1, mesh.n1)
+    pseudo_v = zeros(mesh.l1, mesh.m1, mesh.n1)
+    pseudo_w = zeros(mesh.l1, mesh.m1, mesh.n1)
 
-    # vector to array
-    vector_to_phi!(x, velocity.p, mesh; phisolution = array_pc)
+    u = phi_to_vector(velocity.u, mesh)
+    v = phi_to_vector(velocity.v, mesh)
+    w = phi_to_vector(velocity.w, mesh)
 
-    # pressure correction gradient (result = vector)
-    vector_pcx, vector_pcy, vector_pcz = pressure_phi_gradient(velocity.p, mesh; phisolution = array_pc, T = T)
-
-    # vector to array (easy manipulation for next step)
-    vector_to_phi!(vector_pcx, velocity.p, mesh; phisolution = array_gradpcx)
-    vector_to_phi!(vector_pcy, velocity.p, mesh; phisolution = array_gradpcy)
-    vector_to_phi!(vector_pcz, velocity.p, mesh; phisolution = array_gradpcz)
-
+    asum_u = 0.0
+    asum_v = 0.0
+    asum_w = 0.0
 
     for i in 1:mesh.l1
         for j in 1:mesh.m1
             for k in 1:mesh.n1
-                if velocity.u.onoff[i,j,k]
+                if (velocity.u.onoff[i,j,k] == 1.0)
+
                     id = velocity.u.gIndex[i,j,k]
-                    array_DUx[i,j,k] = 1.0/AU[id,id]
-                else
-                    array_DUx[i,j,k] = 0.0
-                end
 
-                if velocity.v.onoff[i,j,k]
-                    id = velocity.v.gIndex[i,j,k]
-                    array_DUy[i,j,k] = 1.0/AV[id,id]
-                else
-                    array_DUy[i,j,k] = 0.0
-                end
+                    asum_u = 0.0
+                    asum_v = 0.0
 
-                if velocity.w.onoff[i,j,k]
-                    id = velocity.w.gIndex[i,j,k]
-                    array_DUz[i,j,k] = 1.0/AW[id,id]
-                else
-                    array_DUz[i,j,k] = 0.0
+                    if i != 1
+                        if (velocity.u.onoff[i-1,j] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i-1,j,k]] * u[velocity.u.gIndex[i-1,j,k]]
+                            asum_v += AV[id, velocity.v.gIndex[i-1,j,k]] * v[velocity.v.gIndex[i-1,j,k]]
+                            asum_w += AW[id, velocity.v.gIndex[i-1,j,k]] * w[velocity.w.gIndex[i-1,j,k]]
+                        end
+                    end
+
+                    if i != mesh.l1
+                        if (velocity.u.onoff[i+1,j] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i+1,j,k]] * u[velocity.u.gIndex[i+1,j,k]]
+                            asum_v += AV[id, velocity.v.gIndex[i+1,j,k]] * v[velocity.v.gIndex[i+1,j,k]]
+                            asum_w += AW[id, velocity.v.gIndex[i+1,j,k]] * w[velocity.w.gIndex[i+1,j,k]]
+                        end
+                    end
+
+                    if j != 1
+                        if (velocity.u.onoff[i,j-1,k] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i,j-1,k]] * u[velocity.u.gIndex[i,j-1,k]]
+                            asum_v += AV[id, velocity.v.gIndex[i,j-1,k]] * v[velocity.v.gIndex[i,j-1,k]]
+                            asum_w += AW[id, velocity.v.gIndex[i,j-1,k]] * w[velocity.w.gIndex[i,j-1,k]]
+                        end
+                    end
+
+                    if j != mesh.m1
+                        if (velocity.u.onoff[i,j+1,k] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i,j+1,k]] * u[velocity.u.gIndex[i,j+1,k]]
+                            asum_v += AV[id, velocity.v.gIndex[i,j+1,k]] * v[velocity.v.gIndex[i,j+1,k]]
+                            asum_w += AW[id, velocity.v.gIndex[i,j+1,k]] * w[velocity.w.gIndex[i,j+1,k]]
+                        end
+                    end
+
+                    if k != 1
+                        if (velocity.u.onoff[i,j,k-1] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i,j,k-1]] * u[velocity.u.gIndex[i,j,k-1]]
+                            asum_v += AV[id, velocity.v.gIndex[i,j,k-1]] * v[velocity.v.gIndex[i,j,k-1]]
+                            asum_w += AW[id, velocity.v.gIndex[i,j,k-1]] * w[velocity.w.gIndex[i,j,k-1]]
+                        end
+                    end
+
+                    if k != mesh.n1
+                        if (velocity.u.onoff[i,j,k+1] == 1.0)
+                            asum_u += AU[id, velocity.u.gIndex[i,j,k+1]] * u[velocity.u.gIndex[i,j,k+1]]
+                            asum_v += AV[id, velocity.v.gIndex[i,j,k+1]] * v[velocity.v.gIndex[i,j,k+1]]
+                            asum_w += AW[id, velocity.v.gIndex[i,j,k+1]] * w[velocity.w.gIndex[i,j,k+1]]
+                        end
+                    end
+
+
+                    if full
+                        if i > 2
+                            if (velocity.u.onoff[i-2,j] == 1.0)
+                                asum_u += AU[id, velocity.u.gIndex[i-2,j,k]] * u[velocity.u.gIndex[i-2,j,k]]
+                                asum_v += AV[id, velocity.v.gIndex[i-2,j,k]] * v[velocity.v.gIndex[i-2,j,k]]
+                                asum_w += AW[id, velocity.v.gIndex[i-2,j,k]] * w[velocity.w.gIndex[i-2,j,k]]
+                            end
+                        end
+
+                        if i < mesh.l1-1
+                            if (velocity.u.onoff[i+2,j] == 1.0)
+                                asum_u += AU[id, velocity.u.gIndex[i+2,j,k]] * u[velocity.u.gIndex[i+2,j,k]]
+                                asum_v += AV[id, velocity.v.gIndex[i+2,j,k]] * v[velocity.v.gIndex[i+2,j,k]]
+                                asum_w += AW[id, velocity.v.gIndex[i+2,j,k]] * w[velocity.w.gIndex[i+2,j,k]]
+                            end
+                        end
+
+                        if j > 2
+                            if (velocity.u.onoff[i,j-2,k] == 1.0)
+                                asum_u += AU[id, velocity.u.gIndex[i,j-2,k]] * u[velocity.u.gIndex[i,j-2,k]]
+                                asum_v += AV[id, velocity.v.gIndex[i,j-2,k]] * v[velocity.v.gIndex[i,j-2,k]]
+                                asum_w += AW[id, velocity.v.gIndex[i,j-2,k]] * w[velocity.w.gIndex[i,j-2,k]]
+                            end
+                        end
+
+                        if j < mesh.m1-1
+                            if (velocity.u.onoff[i,j+2,k] == 1.0)
+                                asum_u += AU[id, velocity.u.gIndex[i,j+2,k]] * u[velocity.u.gIndex[i,j+2,k]]
+                                asum_v += AV[id, velocity.v.gIndex[i,j+2,k]] * v[velocity.v.gIndex[i,j+2,k]]
+                                asum_w += AW[id, velocity.v.gIndex[i,j+2,k]] * w[velocity.w.gIndex[i,j+2,k]]
+                            end
+                        end
+
+                        if k > 2
+                            if (velocity.u.onoff[i,j,k-2] == 1.0)
+                                asum_u += AU[id, velocity.u.gIndex[i,j,k-2]] * u[velocity.u.gIndex[i,j,k-2]]
+                                asum_v += AV[id, velocity.v.gIndex[i,j,k-2]] * v[velocity.v.gIndex[i,j,k-2]]
+                                asum_w += AW[id, velocity.v.gIndex[i,j,k-2]] * w[velocity.w.gIndex[i,j,k-2]]
+                            end
+                        end
+
+                        if k < mesh.n1-1
+                            if (velocity.u.onoff[i,j,k+2] == 1.0)
+                                asum_u += AU[id, velocity.u.gIndex[i,j,k+2]] * u[velocity.u.gIndex[i,j,k+2]]
+                                asum_v += AV[id, velocity.v.gIndex[i,j,k+2]] * v[velocity.v.gIndex[i,j,k+2]]
+                                asum_w += AW[id, velocity.v.gIndex[i,j,k+2]] * w[velocity.w.gIndex[i,j,k+2]]
+                            end
+                        end
+
+                    end
+
+                    pseudo_u[i,j,k] = (bU[id] - asum_u)/(AU[id,id])
+                    pseudo_v[i,j,k] = (bV[id] - asum_v)/(AV[id,id])
+                    pseudo_w[i,j,k] = (bW[id] - asum_w)/(AW[id,id])
+
                 end
             end
         end
     end
 
-    # Pressure
-    velocity.p.eval = velocity.p.eval + (relaxP * array_pc)
-
-    # Velocity
-    velocity.u.eval = velocity.u.eval - (array_DUx .* array_gradpcx)
-    velocity.v.eval = velocity.v.eval - (array_DUy .* array_gradpcy)
-    velocity.w.eval = velocity.w.eval - (array_DUz .* array_gradpcz)
-
-    # Face Velocity - U
-    for i in 2:mesh.l1
-        for j in 1:mesh.m1
-            for k in 1:mesh.n1
-                if velocity.u.onoff[i,j,k] && velocity.u.onoff[i-1,j,k]
-                    D1 = mesh.vol[i,j,k]/AU[velocity.u.gIndex[i,j,k],velocity.u.gIndex[i,j,k]]
-                    D2 = mesh.vol[i-1,j,k]/AU[velocity.u.gIndex[i-1,j,k],velocity.u.gIndex[i-1,j,k]]
-
-                    num = D1 * (mesh.dx[i-1] * 0.5) + D2 * (mesh.dx[i] * 0.5)
-                    den = 0.5 * (mesh.dx[i-1] + mesh.dx[i])
-                    Df = num / den
-
-                    num = array_pc[i,j,k] - array_pc[i-1,j,k]
-                    den = mesh.x[i] - mesh.x[i-1]
-                    grad_press_face = num / den
-
-                    press = Df * grad_press_face
-
-                    velocity.fValues.uFace[i,j,k] = velocity.fValues.uFace[i,j,k] - press
-                end
-            end
-        end
-    end
-
-    # Face Velocity - V
-    for i in 1:mesh.l1
-        for j in 2:mesh.m1
-            for k in 1:mesh.n1
-                if velocity.v.onoff[i,j,k] && velocity.v.onoff[i,j-1,k]
-                    D1 = mesh.vol[i,j,k]/AV[velocity.v.gIndex[i,j,k],velocity.v.gIndex[i,j,k]]
-                    D2 = mesh.vol[i,j-1,k]/AV[velocity.v.gIndex[i,j-1,k],velocity.v.gIndex[i,j-1,k]]
-
-                    num = D1 * (mesh.dy[j-1] * 0.5) + D2 * (mesh.dy[j] * 0.5)
-                    den = 0.5 * (mesh.dy[j-1] + mesh.dy[j])
-                    Df = num / den
-
-                    num = array_pc[i,j,k] - array_pc[i,j-1,k]
-                    den = mesh.y[j] - mesh.y[j-1]
-                    grad_press_face = num / den
-
-                    press = Df * grad_press_face
-
-                    velocity.fValues.vFace[i,j,k] = velocity.fValues.vFace[i,j,k] - press
-                end
-            end
-        end
-    end
-
-    # Face Velocity - W
-    for i in 1:mesh.l1
-        for j in 1:mesh.m1
-            for k in 2:mesh.n1
-                if velocity.w.onoff[i,j,k] && velocity.w.onoff[i,j,k-1]
-                    D1 = mesh.vol[i,j,k]/AW[velocity.w.gIndex[i,j,k],velocity.w.gIndex[i,j,k]]
-                    D2 = mesh.vol[i,j,k-1]/AW[velocity.w.gIndex[i,j,k-1],velocity.w.gIndex[i,j,k-1]]
-
-                    num = D1 * (mesh.dz[k-1] * 0.5) + D2 * (mesh.dz[k] * 0.5)
-                    den = 0.5 * (mesh.dz[k-1] + mesh.dz[k])
-                    Df = num / den
-
-                    num = array_pc[i,j,k] - array_pc[i,j,k-1]
-                    den = mesh.z[k] - mesh.z[k-1]
-                    grad_press_face = num / den
-
-                    press = Df * grad_press_face
-
-                    velocity.fValues.wFace[i,j,k] = velocity.fValues.wFace[i,j,k] - press
-                end
-            end
-        end
-    end
-
-    return nothing
+    return pseudo_u, pseudo_v
 end
